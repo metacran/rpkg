24ff04b20c2881f3fdc76d25ad17a05be09f46ac
Gabor Csardi <csardi.gabor@gmail.com>
2015-03-16 16:26:49 -0400

    Internal docs/plan update

--- R/rpkg-package.R
+++ R/rpkg-package.R
@@ -7,40 +7,40 @@ NULL
 
 ## API:
 ##
-## pkg_install    - install
-## pkg_remove     - remove
-## pkg_download   - download (but do not install) package
-## pkg_update     - update
-## pkg_reinstall  - reinstall same version
-## pkg_clean_deps - remove packages that were installed
-##                  only as dependencies and are not needed any more
-## pkg_list       - list installed packages
-## pkg_search     - search for packages
-## pkg_info       - display information about package
-## pkg_outdated   - show installed packages that are outdated
-## pkg_doctor     - sanity checks
-## pkg_paths      - set/get package directories
-## pkg_tree       - package(s) and their dependency tree
-## pkg_bug        - report bug for a package
-## pkg_browse     - home page of a package (or rpkg itself), or repo URL
+## * pkg_install    - install
+##   pkg_remove     - remove
+## * pkg_download   - download (but do not install) package
+##   pkg_upgrade    - upgrade
+##   pkg_reinstall  - reinstall same version
+##   pkg_clean_deps - remove packages that were installed
+##                    only as dependencies and are not needed any more
+## * pkg_list       - list installed packages
+##   pkg_search     - search for packages
+##   pkg_info       - display information about package
+##   pkg_outdated   - show installed packages that are outdated
+##   pkg_doctor     - sanity checks
+## * pkg_paths      - set/get package directories
+## * pkg_tree       - package(s) and their dependency tree
+##   pkg_bug        - report bug for a package
+##   pkg_browse     - home page of a package (or rpkg itself), or repo URL
 ##
-## Configuration:
+## Configuration (later):
 ##
 ## pkg_config    - get/set configuration options
 ## 
-## Account management:
+## Account management (later):
 ##
 ## pkg_login     - log in to the rpkg service
 ## pkg_logout    - log out
 ## pkg_whoami    - print logged in user
 ## pkg_adduser   - add a new user
 ##
-## Logging and statistics
+## Logging and statistics (later):
 ##
 ## pkg_log       - entries from the log, about a package or everything
 ## pkg_downloads - download statistics about a package or general
 ##
-## Package creation, binaries and distribution
+## Package creation, binaries and distribution (later):
 ##
 ## pkg_create       - create an empty package
 ## pkg_submit       - submit it to rpkg
@@ -53,6 +53,6 @@ NULL
 ## pkg_make_centos
 ## ...
 ##
-## Repo management
+## Repo management (later):
 ##
 ## later...
